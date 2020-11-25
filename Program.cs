b3b2febec0c1e7d1123494f32ff3640c3c2640bc
Juan Cruz Fiant <jfiant@desarrollosnea.com>
2020-11-25 11:59:44 -0300

    Add notification page

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Append.Blazor.Notifications;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +19,7 @@ namespace BlazorPwa
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddNotifications();
 
             await builder.Build().RunAsync();
         }
--- wwwroot/service-worker.published.js
+++ wwwroot/service-worker.published.js
@@ -45,6 +45,7 @@ async function onInstall(event) {
         .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
         .map(asset => new Request(asset.url));
     await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
+    self.skipWaiting();
 }
 
 async function onActivate(event) {
